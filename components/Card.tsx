bd46d940845e02deebc267d9e9a0bba4f6ef7902
Aidan Timson <contact@timmo.xyz>
2020-09-19 13:07:00 +0100

    Add tags (#1)
    * Add tags
    
    * Add interface

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,11 +4,16 @@ import Link from "next/link";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
+import Chip from "@material-ui/core/Chip";
 import MuiCard from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 
 import { getApiMediaUrl } from "../lib/api";
 import useStyles from "../assets/jss/components/card";
+
+interface Tag {
+  name: string;
+}
 
 const Card = ({ article }) => {
   const classes = useStyles();
@@ -38,6 +43,13 @@ const Card = ({ article }) => {
               component="span"
               variant="subtitle1">
               <Moment format="Do MMMM YYYY">{article.published_at}</Moment>
+            </Typography>
+            <Typography component="div">
+              {article.tags
+                .sort((a: Tag, b: Tag) => (a.name > b.name ? 1 : -1))
+                .map((tag: Tag, index: number) => (
+                  <Chip key={index} label={tag.name} />
+                ))}
             </Typography>
           </CardContent>
         </MuiCard>
