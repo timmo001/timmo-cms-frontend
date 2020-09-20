703f05e9d39340dfc84b0e0e2e40b6a195a37e85
Aidan Timson <contact@timmo.xyz>
2020-09-20 16:53:51 +0100

    Add manifest and icons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -60,7 +60,14 @@ const Layout = (props) => {
     <>
       <Head>
         <title>Timmo</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#009688">
         <meta name="description" content="Aidan Timson aka Timmo" />
+        <meta name="msapplication-TileColor" content="#009688">
+        <meta name="theme-color" content="#009688">
       </Head>
       <NoSsr>
         <ThemeProvider theme={theme}>
